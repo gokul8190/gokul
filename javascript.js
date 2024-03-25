74279fd7bc948c1725c4a25aa3e454ed0625ba91
gokul8190 <gokulindu90@gmail.com>
2024-03-25 13:00:56 +0530

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,44 @@
+//const, var, let
+
+const PI = 3.1415;
+var x = 1;
+console.log(x)
+var y = 1.4;
+var z = "hello";
+console.log(y);
+var a = [a, y, z];
+var1st = [1, "3",[1,0], 4,8, true];
+console.log("1st");
+var obj = {
+    name: "gg",
+    job: "hacker",
+    age: 19,
+    adress: "banglore"
+}
+console.log(obj)
+
+let phone = {
+    company: {
+        name: "vivo",
+        country: "india"
+        } ,
+        name: "y35",
+        size: 5.5,
+        weight: 150,
+        color: ["yellow", "blue", "red"]
+}
+
+let str1 = "hello";
+let str2 = "world";
+let str3 = "hello";
+world ;
+console.log(str3)
+
+let outcome = [[1, 3, 2, 4, 5, 6],
+               [4, 5, 3, 5, 4, 1],
+               [3, 4, 3, 2, 1, 5],
+               [2, 5, 6, 1, 3, 2],
+               [4, 4, 3, 6, 3, 1]]
+
+console.log(outcome[1][3])//5
+console.log(outcome[4][3])//8
